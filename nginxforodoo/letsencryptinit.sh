ba06ac2143abeb86f45115f0192c88fae3e28b94
mlebreuil <mlebreuil@users.noreply.github.com>
2017-11-22 23:14:45 +0100

    Update and rename letsencrypt.sh to letsencryptinit.sh

--- nginxforodoo/letsencryptinit.sh
+++ nginxforodoo/letsencryptinit.sh
@@ -4,5 +4,5 @@ sudo docker run -it --rm -p 443:443 -p 80:80 --name certbot \
             -v "./nginxforodoo/ssl/:/etc/letsencrypt" \
             -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
             certbot/certbot certonly\
-            --webroot \
-            --webroot-path ./nginxforodoo/www/odoo.famillelebreuil.net/
+            --manual \
+            -d odoo.famillelebreuil.net/
