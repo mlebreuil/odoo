60e6ab646c475ad4970c77085260e4d5e7582b74
mlebreuil <mlebreuil@users.noreply.github.com>
2017-11-22 23:47:47 +0100

    Update letsencryptinit.sh

--- nginxforodoo/letsencryptinit.sh
+++ nginxforodoo/letsencryptinit.sh
@@ -1,8 +1,8 @@
 # Pull the docker image
 sudo docker pull certbot/certbot
 sudo docker run -it --rm -p 443:443 -p 80:80 --name certbot \
-            -v "./nginxforodoo/ssl/:/etc/letsencrypt" \
-            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            -v /home/lebreuilm/ssl/:/etc/letsencrypt \
+            -v /var/lib/letsencrypt:/var/lib/letsencrypt \
             certbot/certbot certonly\
-            --manual \
+            --standalone \
             -d odoo.famillelebreuil.net
